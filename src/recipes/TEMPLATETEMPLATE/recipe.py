080d1a14a0ea89f3eba60d9b66ba2e4c2f509267
Tobias Richter <tobias.richter@esss.se>
2016-11-22 13:21:52 +0100

    working version of the newfeature script
    at least it retrieves a valid id from the idservice
    (provided all dependencies are met)

---
+++ src/recipes/TEMPLATETEMPLATE/recipe.py
@@ -0,0 +1,35 @@
+class recipe:
+    """
+    @TITLE@
+
+    Proposed by: @EMAIL@
+    Please change this docstring to something meaningful
+    that describes your NeXus feature.
+    """
+
+    def __init__(self, filedesc, entrypath):
+        """
+        Recipes are required to set a descriptive self.title
+
+        :param filedesc: h5py file object of the NeXus/HDF5 file
+        :param entrypath: path of the entry containing this feature
+        """
+
+        raise Exception("unedited template code found")
+
+        self.file = filedesc
+        self.entry = entrypath
+        self.title = "@TITLE@"
+
+    def process(self):
+        """
+        Recipes need to implement this method and return information which
+        is useful to a user and instructive to a person reading the code.
+        See some of the recommended examples for inspriration what to return.
+
+        :return: the essence of the information recorded in this feature
+        """
+
+        raise Exception("unedited template code found")
+
+        return []
