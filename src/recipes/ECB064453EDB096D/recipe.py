37bdce340597306420b33ffb78c14b09e65ef629
Matthew D Jones <matthew.d.jones@tessella.com>
2017-09-27 15:31:16 +0100

    Instead of executing examples, return a class with examples as methods

--- src/recipes/ECB064453EDB096D/recipe.py
+++ src/recipes/ECB064453EDB096D/recipe.py
@@ -35,107 +35,112 @@ def check_nevents(context, nx_event_data, item, fails):
             % item)
 
 
-def isotime_to_unixtime_in_seconds(isotime):
-    utc_dt = datetime.strptime(isotime, '%Y-%m-%dT%H:%M:%S')
-    # convert UTC datetime to seconds since the Epoch
-    return (utc_dt - datetime(1970, 1, 1)).total_seconds()
+class NXevent_dataExamples:
+    def __init__(self):
+        pass
 
+    @staticmethod
+    def get_pulse_index_of_event(nx_event_data, nth_event):
+        """
+        Find the pulse index that the nth_event occurred in
+        :param: nx_event_data: An NXevent_data group which was found in the file
+        :param nth_event: The Nth detection event in the group
+        :return: pulse index of the nth_event
+        """
+        # Find index of the last element which has event_index lower than the nth_event
+        # this is the index of the pulse (frame) which the nth_event falls in
+        event_index = nx_event_data['event_index'][...]
+        for i, event_index_for_pulse in enumerate(event_index):
+            if event_index_for_pulse > nth_event:
+                return i - 1
 
-def convert_to_seconds(event_offset, time_unit):
-    if time_unit in ['seconds', 'second', 's']:
-        return event_offset
-    elif time_unit in ['milliseconds', 'millisecond', 'ms']:
-        return event_offset * 1e-3
-    elif time_unit in ['microseconds', 'microsecond', 'us']:
-        return event_offset * 1e-6
-    elif time_unit in ['nanoseconds', 'nanosecond', 'ns']:
-        return event_offset * 1e-9
-    else:
-        raise ValueError('Unrecognised time unit in event_time_offset')
+    def get_time_neutron_detected(self, nx_event_data, nth_event):
+        """
+        Use offset and time units attributes to find the absolute time
+        that neutron with index of event_number to hit the detector
+        :param: nx_event_data: An NXevent_data group which was found in the file
+        :param nth_event: The Nth detection event in the group
+        :return: Absolute time of neutron event detection in ISO8601 format
+        """
+        if "event_time_offset" in nx_event_data.keys() and nx_event_data['event_time_offset'][...].size > (
+                    nth_event + 1):
+            # Get absolute pulse time in seconds since epoch
+            pulse_index = self.get_pulse_index_of_event(nx_event_data, nth_event)
+            pulse_start_time_seconds = self._convert_to_seconds(nx_event_data['event_time_zero'][pulse_index],
+                                                                nx_event_data['event_time_zero'].attrs['units'])
+            pulse_start_offset = self._isotime_to_unixtime_in_seconds(nx_event_data['event_time_zero'].attrs['offset'])
+            pulse_absolute_seconds = pulse_start_time_seconds + pulse_start_offset
 
+            # Get event time in seconds relative to pulse time
+            event_offset = nx_event_data['event_time_offset'][nth_event]
+            event_offset_seconds = self._convert_to_seconds(event_offset,
+                                                            nx_event_data['event_time_offset'].attrs['units'])
 
-def get_pulse_index_of_event(nx_event_data, nth_event):
-    """
-    Find the pulse index that the nth_event occurred in
-    :param: nx_event_data: An NXevent_data group which was found in the file
-    :param nth_event: The Nth detection event in the group
-    :return: pulse index of the nth_event
-    """
-    # Find index of the last element which has event_index lower than the nth_event
-    # this is the index of the pulse (frame) which the nth_event falls in
-    event_index = nx_event_data['event_index'][...]
-    for i, event_index_for_pulse in enumerate(event_index):
-        if event_index_for_pulse > nth_event:
-            return i - 1
+            # Calculate absolute event time in seconds since epoch
+            absolute_event_time_seconds = pulse_absolute_seconds + event_offset_seconds
+            # and convert to a readable string in ISO8601 format
+            absolute_event_time_iso = datetime.fromtimestamp(absolute_event_time_seconds, tz=UTC()).isoformat()
+            return absolute_event_time_iso
 
+    def get_slice_of_events_by_time(self, nx_event_data, event_index):
+        """
+        Get cued slice of events which includes the event index specified.
 
-def get_time_neutron_detected(nx_event_data, nth_event):
-    """
-    Use offset and time units attributes to find the absolute time
-    that neutron with index of event_number to hit the detector
-    :param: nx_event_data: An NXevent_data group which was found in the file
-    :param nth_event: The Nth detection event in the group
-    :return: Absolute time of neutron event detection in ISO8601 format
-    """
-    if "event_time_offset" in nx_event_data.keys() and nx_event_data['event_time_offset'][...].size > (nth_event + 1):
-        # Get absolute pulse time in seconds since epoch
-        pulse_index = get_pulse_index_of_event(nx_event_data, nth_event)
-        pulse_start_time_seconds = convert_to_seconds(nx_event_data['event_time_zero'][pulse_index],
-                                                      nx_event_data['event_time_zero'].attrs['units'])
-        pulse_start_offset = isotime_to_unixtime_in_seconds(nx_event_data['event_time_zero'].attrs['offset'])
-        pulse_absolute_seconds = pulse_start_time_seconds + pulse_start_offset
+        cue_index and cue_timestamp_zero allow easy efficient access to a slice of event
+        data between two timestamps. This slice might, for example, correspond to when a
+        control parameter was changed or data which were received from a network stream
+        in a single message.
 
-        # Get event time in seconds relative to pulse time
-        event_offset = nx_event_data['event_time_offset'][nth_event]
-        event_offset_seconds = convert_to_seconds(event_offset, nx_event_data['event_time_offset'].attrs['units'])
+        :param nx_event_data: An NXevent_data group which was found in the file
+        :param event_index: event_index which falls inside the range for a cued slice of events
+        :return: tuple of lists of event_id and event_time_offset for events in a cued slice
+        """
+        # Find cue entry which falls just before the event_index we are looking for
+        cue_i = self._get_last_index_before_larger_than_target(nx_event_data['cue_index'], event_index)
+        # Index range of slice
+        start_i = nx_event_data['cue_index'][cue_i]
+        end_i = nx_event_data['cue_index'][cue_i + 1]
+        return nx_event_data['event_id'][start_i:end_i], nx_event_data['event_time_offset'][start_i:end_i]
 
-        # Calculate absolute event time in seconds since epoch
-        absolute_event_time_seconds = pulse_absolute_seconds + event_offset_seconds
-        # and convert to a readable string in ISO8601 format
-        absolute_event_time_iso = datetime.fromtimestamp(absolute_event_time_seconds, tz=UTC()).isoformat()
-        return absolute_event_time_iso
+    @staticmethod
+    def _get_last_index_before_larger_than_target(search_dataset, target, start_index=0, end_index=-1):
+        """
+        Find the index of the last element in the search_dataset which has a value
+        smaller than target.
+        Search space can be reduced with start_index and end_index to avoid
+        loading the entire dataset from file. Event datasets are often larger
+        than available memory.
+        :param search_dataset: dataset to search
+        :param target: target value
+        :param start_index: index to start searching at
+        :param end_index: index to search until
+        :return: last index before the value exceeds the target
+        """
+        last_i = 0
+        for i, value in enumerate(search_dataset[start_index:end_index]):
+            if target > value:
+                return max(0, (i + start_index) - 1)
+            last_i = i
+        return last_i - 1
 
+    @staticmethod
+    def _isotime_to_unixtime_in_seconds(isotime):
+        utc_dt = datetime.strptime(isotime, '%Y-%m-%dT%H:%M:%S')
+        # convert UTC datetime to seconds since the Epoch
+        return (utc_dt - datetime(1970, 1, 1)).total_seconds()
 
-def get_last_index_before_larger_than_target(search_dataset, target, start_index=0, end_index=-1):
-    """
-    Find the index of the last element in the search_dataset which has a value
-    smaller than target.
-    Search space can be reduced with start_index and end_index to avoid
-    loading the entire dataset from file. Event datasets are often larger
-    than available memory.
-    :param search_dataset: dataset to search
-    :param target: target value
-    :param start_index: index to start searching at
-    :param end_index: index to search until
-    :return: last index before the value exceeds the target
-    """
-    last_i = 0
-    for i, value in enumerate(search_dataset[start_index:end_index]):
-        if target > value:
-            return max(0, (i + start_index) - 1)
-        last_i = i
-    return last_i - 1
-
-
-def get_slice_of_events_by_time(nx_event_data, event_index):
-    """
-    Get cued slice of events which includes the event index specified.
-
-    cue_index and cue_timestamp_zero allow easy efficient access to a slice of event
-    data between two timestamps. This slice might, for example, correspond to when a
-    control parameter was changed or data which were received from a network stream
-    in a single message.
-
-    :param nx_event_data: An NXevent_data group which was found in the file
-    :param event_index: event_index which falls inside the range for a cued slice of events
-    :return: tuple of lists of event_id and event_time_offset for events in a cued slice
-    """
-    # Find cue entry which falls just before the event_index we are looking for
-    cue_i = get_last_index_before_larger_than_target(nx_event_data['cue_index'], event_index)
-    # Index range of slice
-    start_i = nx_event_data['cue_index'][cue_i]
-    end_i = nx_event_data['cue_index'][cue_i+1]
-    return nx_event_data['event_id'][start_i:end_i], nx_event_data['event_time_offset'][start_i:end_i]
+    @staticmethod
+    def _convert_to_seconds(event_offset, time_unit):
+        if time_unit in ['seconds', 'second', 's']:
+            return event_offset
+        elif time_unit in ['milliseconds', 'millisecond', 'ms']:
+            return event_offset * 1e-3
+        elif time_unit in ['microseconds', 'microsecond', 'us']:
+            return event_offset * 1e-6
+        elif time_unit in ['nanoseconds', 'nanosecond', 'ns']:
+            return event_offset * 1e-9
+        else:
+            raise ValueError('Unrecognised time unit in event_time_offset')
 
 
 VALIDATE = {
@@ -201,25 +206,12 @@ class recipe:
         self.entry = entrypath
         self.title = "NXevent_data"
 
-    @staticmethod
-    def execute_examples(nx_event_data):
-        try:
-            absolute_detection_time = get_time_neutron_detected(nx_event_data, 3)
-            event_ids, event_time_offsets = get_slice_of_events_by_time(nx_event_data, 400)
-        except Exception:
-            # An exception here is not a validation failure so don't pass it upstream
-            pass
-
     def process(self):
         nx_event_data = _NXevent_dataFinder()
         nx_event_data_list = nx_event_data.get_NXevent_data(self.file, self.entry)
         if len(nx_event_data_list) == 0:
             raise AssertionError("No NXevent_data entries found")
-        entries = []
         for nx_event_data_entry in nx_event_data_list:
-            validation_fails = validate(nx_event_data_entry)
-            entries.append(validation_fails)
+            validate(nx_event_data_entry)
 
-            if not validation_fails:
-                self.execute_examples(nx_event_data_entry)
-        return entries
+        return NXevent_dataExamples
