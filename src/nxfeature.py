6c533e5e06de7158533ece331a4e5e924bf713ad
Jack Harper <jack.harper@stfc.ac.uk>
2017-10-25 15:11:33 +0100

    adding junit xml factory

--- src/nxfeature.py
+++ src/nxfeature.py
@@ -13,6 +13,41 @@ RECIPIE_DIR = os.path.dirname(os.path.realpath(__file__)) + "/recipes"
 sys.path.append(RECIPIE_DIR)
 
 
+class TestBody:
+    def __init__(self, failure_type="", failure_message=""):
+        self.failure = failure_type
+        self.message = failure_message
+
+    def get_str(self):
+        return "\n\t\t\t<failure type=\"{}\">{}</failure>".format(self.failure, self.message) if (self.failure and self.message) else "\n"
+
+
+class TestCase:
+    def __init__(self, class_name, name, body=TestBody()):
+        self.class_name = class_name
+        self.name = name
+        self.body = body
+
+    def get_str(self):
+        return "\t\t<testcase classname=\"{}\" name=\"{}\">{}\n\t\t</testcase>".format(self.class_name, self.name, self.body.get_str())
+
+
+class JUnitFactory:
+    def __init__(self):
+        self.test_cases = []
+
+    def write(self, xml_file):
+        output_str = "<testsuites>\n\t<testsuite name=\"features\" tests=\"" + str(len(self.test_cases)) + "\">\n"
+        for test in self.test_cases:
+            output_str += test.get_str() + "\n"
+        output_str += "\t</testsuite>\n</testsuites>"
+        with open(xml_file, "w+") as file:
+            file.write(output_str)
+
+    def add_test_case(self, feat, message, failure_type=None, failure_message=None):
+        self.test_cases.append(TestCase(feat, message, TestBody(failure_type, failure_message)))
+
+
 class InsaneEntryWithFeatures:
     def __init__(self, nxsfile, entrypath, featurearray):
         self.nxsfile = nxsfile
@@ -71,6 +106,7 @@ class AllFeatureDiscoverer:
                 pass
         return ent
 
+
 class SingleFeatureDiscoverer:
     def __init__(self, nxsfile, feature):
         self.file = h5py.File(nxsfile, 'r')
@@ -96,8 +132,9 @@ if __name__ == '__main__':
                         default=False)
     parser.add_argument("-f", "--feature", dest="feature", help="Test file against a defined feature",
                       default=None)
-    parser.add_argument("-v", "--verbose", dest="verbose", help="Include full stacktraces of failues", action="store_true",
+    parser.add_argument("-v", "--verbose", dest="verbose", help="Include full stacktraces of failures", action="store_true",
                         default=False)
+    parser.add_argument("-x", "--xml", dest="xml", help="XML file to write the junit output to", default="./junit_output.xml")
     parser.add_argument("nexusfile", help="Nexus file to test")
 
     args = parser.parse_args()
@@ -113,8 +150,9 @@ if __name__ == '__main__':
           disco = AllFeatureDiscoverer(args.nexusfile)
       else:
           disco = InsaneFeatureDiscoverer(args.nexusfile)
-
+    factory = JUnitFactory()
     for entry in disco.entries():
+        pass_list = []
         fail_list = []
         error_list = []
 
@@ -122,20 +160,26 @@ if __name__ == '__main__':
         for feat in entry.features():
             try:
                 response = entry.feature_response(feat)
+                pass_list.append((feat, response))
                 print("\t%s (%d) %s" % (entry.feature_title(feat), feat, response))
             except AssertionError as ae:
                 fail_list.append((feat, ae.message))
             except Exception as e:
                 error_list.append((feat, traceback.format_exc()))
 
+        output = str()
+        for feat, message in pass_list:
+            factory.add_test_case(feat, message)
+
         if len(fail_list) > 0:
             print("\n\tThe following features failed to validate:")
             for feat, message in fail_list:
                     print("\t\t%s (%d) is invalid with the following errors:" % (entry.feature_title(feat), feat))
                     print("\t\t\t" + message.replace('\n', '\n\t\t'))
+                    factory.add_test_case(entry.feature_title(feat), feat, "AssertionError", message)
 
         if len(error_list) > 0:
-            print("\n\tThe following features had unexpected errors (Are you running windows?):")
+            print("\n\tThe following features had unexpected errors (Are you running windows?):") #build error
             for feat, message in error_list:
                 try:
                     print("\t\t%s (%d) had an unexpected error" % (entry.feature_title(feat), feat))
@@ -144,3 +188,5 @@ if __name__ == '__main__':
                 except:
                     print("\t\tFeature (%d) could not be found" % (feat))
         print("\n")
+
+    factory.write(args.xml)
